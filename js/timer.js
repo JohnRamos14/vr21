ada4e11b4529987ffea6187c999a4813f91b5a61
JohnRamos14 <99770479+JohnRamos14@users.noreply.github.com>
2022-12-05 09:31:40 -0800

    added reset func to timer

--- js/timer.js
+++ js/timer.js
@@ -2,16 +2,22 @@ let timerId;
 let counter = 0;
 
 function start() {
-    if(!timerId){
+  if (!timerId) {
     timerId = setInterval(() => {
-        counter++;
-        document.querySelector(".count").innerText = counter;
-        console.log(counter);
+      counter++;
+      document.querySelector(".count").innerText = counter;
+      console.log(counter);
     }, 1000);
-    }
+  }
 }
 
 function stop() {
-        clearInterval(timerId);
-        timerId = null;
-    }
+  clearInterval(timerId);
+  timerId = null;
+}
+
+function reset() {
+  counter = 0;
+  document.querySelector(".count").innerText = counter;
+  stop();
+}
